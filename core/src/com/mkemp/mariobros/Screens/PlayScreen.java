ffe78714b15a64763d284df9f27a1084e2bb174c
mkkemp94 <mkkemp94@gmail.com>
2017-06-24 17:09:52 -0400

    Custom level added and hooked up via code.

--- core/src/com/mkemp/mariobros/Screens/PlayScreen.java
+++ core/src/com/mkemp/mariobros/Screens/PlayScreen.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mkemp.mariobros.MarioBros;
@@ -23,9 +26,14 @@ import static com.mkemp.mariobros.MarioBros.V_WIDTH;
 public class PlayScreen implements Screen {
 
     private MarioBros game;
+
     private OrthographicCamera gameCam;
     private Viewport gamePort;
     private Hud hud;
+
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
 
     // We're sending the game to the screen, so we need a constructor.
     public PlayScreen(MarioBros game) {
@@ -35,6 +43,12 @@ public class PlayScreen implements Screen {
         gameCam = new OrthographicCamera();
         gamePort = new FitViewport(V_WIDTH, V_HEIGHT, gameCam);
         hud = new Hud(game.batch);
+
+        // Handle rendering tiled map.
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("custom_level.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
     }
 
     @Override
@@ -42,8 +56,29 @@ public class PlayScreen implements Screen {
 
     }
 
+    /**
+     * Handle any input to the screen.
+     */
+    public void handleInput(float dt) {
+        if (Gdx.input.isTouched()) {
+            gameCam.position.x += 100 * dt;
+        }
+    }
+
+    /**
+     * This method does all the updating of the game world.
+     */
+    public void update(float dt) {
+        handleInput(dt);
+        gameCam.update();
+        renderer.setView(gameCam);
+    }
+
     @Override
     public void render(float delta) {
+
+        // Calculate any changes.
+        update(delta);
 
         // Clear the screen.
         Gdx.gl.glClearColor(0, 0, 0, 1);
@@ -51,6 +86,8 @@ public class PlayScreen implements Screen {
 
         // Tell the sprite batch to only render what we can see.
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+
+        // Draw the hud.
         hud.stage.draw();
 
         // Open box to start drawing.
@@ -58,6 +95,9 @@ public class PlayScreen implements Screen {
 
         // Close box.
         game.batch.end();
+
+        // Render the map with the updated changes.
+        renderer.render();
     }
 
     @Override
